959b48c2b4abc1f04fa81e429f376dde7f7798d2
Ta Tran Dinh Tien <tatrandinhtien@gmail.com>
2026-06-02 11:40:02 +0700

    add test_motor.c

--- app/test/src/test_motor.c
+++ app/test/src/test_motor.c
@@ -1,12 +1,11 @@
 /**
  * @file test_motor.c
  * @author dt (tien.ta.eswe@gmail.com)
- * @brief testing motor module with FreeRTOS
- * @version 0.4
+ * @brief testing H-Bridge motor control module with FreeRTOS
+ * @version 0.1
  * @date 2026-05-31
- * @copyright Copyright (c) 2026
+ * * @copyright Copyright (c) 2026
  * */
-
 #include "test_motor.h"
 #include "bsp_hbridge.h"
 #include "bsp_log.h"
@@ -20,6 +19,63 @@
 #include <stdio.h>
 #include "cmsis_os2.h"
 
+static void vTestMotorTask(void *argument) {
+    (void)argument;
+
+    bsp_log_printf("[Test] Motor RTOS Task Started!\r\n");
+
+    while (1) {
+        // bsp_log_printf("[Motor] Forwarding: 30%%\r\n");
+        // BSP_Motor_SetSpeed(30);
+        // osDelay(2000);
+
+        // bsp_log_printf("[Motor] Forwarding: 70%%\r\n");
+        // BSP_Motor_SetSpeed(70);
+        // osDelay(2000);
+
+        // bsp_log_printf("[Motor] Forwarding: 99%%\r\n");
+        // BSP_Motor_SetSpeed(99);
+        // osDelay(2000);
+
+        // bsp_log_printf("[Motor] Forwarding 40%%\r\n");
+        // BSP_Motor_SetSpeed(40);
+        // osDelay(1000);
+
+        // bsp_log_printf("[Motor] Stop...\r\n");
+        // BSP_Motor_SetSpeed(0);
+        // osDelay(1500);
+
+        // bsp_log_printf("[Motor] Reverse: -30%%\r\n");
+        // BSP_Motor_SetSpeed(-30);
+        // osDelay(2000);
+
+        // bsp_log_printf("[Motor] Reverse: -70%%\r\n");
+        // BSP_Motor_SetSpeed(-70);
+        // osDelay(2000);
+
+        bsp_log_printf("[Motor] Stop\r\n");
+        BSP_Motor_SetSpeed(0);
+        osDelay(3000);
+    }
+}
+
 void test_motor_run(void) {
-    
+
+    BSP_Motor_Init();
+    bsp_log_init();
+
+    setvbuf(stdout, NULL, _IONBF, 0);
+
+    osKernelInitialize();
+
+    const osThreadAttr_t task_attr = {
+        .name = "TestMotor_Task",
+        .priority = osPriorityNormal,
+        .stack_size = 2048
+    };
+    osThreadNew(vTestMotorTask, NULL, &task_attr);
+
+    osKernelStart();
+
+    while (1) {}
 }
