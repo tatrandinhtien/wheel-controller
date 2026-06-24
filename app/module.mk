b4c57916b81587e038c1f9e6fdc08c687d136a3d
Ta Tran Dinh Tien <tatrandinhtien@gmail.com>
2026-06-24 10:57:49 +0700

    feat(app): add submodule makefile for testing encoder

--- app/module.mk
+++ app/module.mk
@@ -3,9 +3,10 @@ INC += 	-Iapp/inc \
 
 SRCS += app/src/main.c	\
 		app/src/ring_buffer.c	\
-		app/src/wheel_controller.c \
+		app/src/app_wheel_controller.c \
 		app/test/src/test_gpio.c \
 		app/test/src/test_log.c \
 		app/test/src/test_can.c	\
 		app/test/src/test_servo.c \
+		app/test/src/test_encoder.c \
 		app/test/src/test_motor.c
